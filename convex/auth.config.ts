87f978b143ff8397085c9a0ab1b3408e138c1dd3
Sergio <sergio.dussan@gmail.com>
2024-08-22 08:26:02 -0700

    first commmit

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+    providers: [
+      {
+        domain: "https://native-mammal-43.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+               protocol: 'https',
+               hostname: 'lovely-flamingo-139.convex.cloud',
+            },
+            {
+                protocol: 'https',
+                hostname: 'accurate-rhinoceros-122.convex.cloud',
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.clerk.com',
+            },
+        ]
+        
+    }
+};
 
 export default nextConfig;
